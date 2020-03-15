a6ccc94df35126c8a629f0903a6137aaa24609fa
Sergey Lem <sergey.lem@gmail.com>
2020-03-15 23:52:59 +0600

    Modified specification in models

--- models/specification.js
+++ models/specification.js
@@ -11,25 +11,25 @@ const specificationSchema = new mongoose.Schema(
         performance: {
             type: String,
             trim: true,
-            required: true,
+            required: false,
             maxlength: 150  // Apple A13 Bionic
         },
         storage: {
             type: String,
             trim: true,
-            required: true,
+            required: false,
             maxlength: 3 // 64 + "GB"
         },
         camera: {
             type: String,
             trim: true,
-            required: true,
+            required: false,
             maxlength: 100  // 12 MP + 12 MP
         },
         battery: {
             type: String,
             trim: true,
-            required: true,
+            required: false,
             maxlength: 4  // 3110 + "mAh"
         },
         display: {
@@ -41,8 +41,14 @@ const specificationSchema = new mongoose.Schema(
         ram: {
             type: String,
             trim: true,
-            required: true,
+            required: false,
             maxlength: 5  // 4 + "GB"
+        },
+        os: {
+            type: String,
+            trim: true,
+            required: false,
+            maxlength: 12  // Android 
         }
 
         // ,
